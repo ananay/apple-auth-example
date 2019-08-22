b38285ad4080c3d3bf863c08ed521500e44ecf48
Nico Kaiser <nico.kaiser@boerse-go.de>
2019-08-22 22:11:42 +0200

    Fix token and user data retrieval

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const fs = require('fs');
 const config = fs.readFileSync('./config/config.json');
 const AppleAuth = require('apple-auth');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 let auth = new AppleAuth(config, './config/AuthKey.p8');
 
@@ -15,11 +17,22 @@ app.get('/token', (req, res) => {
     res.send(auth._tokenGenerator.generate());
 });
 
-app.get('/auth', async (req, res) => {
+app.post('/auth', bodyParser(), async (req, res) => {
     try {
         console.log( Date().toString() + "GET /auth");
-        const accessToken = await auth.accessToken(req.query.code);
-        res.json(accessToken);
+        const response = await auth.accessToken(req.body.code);
+        const idToken = jwt.decode(response.id_token);
+
+        const user = {};
+        user.id = idToken.sub;
+
+        if (idToken.email) user.email = idToken.email;
+        if (req.body.user) {
+            const { name } = JSON.parse(req.body.user);
+            user.name = name;
+        }
+
+        res.json(user);
     } catch (ex) {
         console.error(ex);
         res.send("An error occurred!");
